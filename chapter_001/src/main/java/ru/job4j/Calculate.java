15ae4960b9990525f64713672c52116338a0073b
artem <artem>
2017-03-21 19:40:30 +0300

    task 4087

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,7 +1,19 @@
 package job4j;
 
+/**
+*Class Calculate решение задачи части 001 урок1.
+*@author shipilov (shipilov.89521049213@mail.ru)
+*@since 21.03.2017
+*/
+
 public class Calculate {
-  public static void main(String[] args){
+	
+	/**
+     * Main.
+     * @param args - args.
+     */
+	 
+  public static void main(String[] args) {
 	  System.out.println("Hello world!");
   }
 }
---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,8 @@
+/**
+* //package for calculator task
+*
+* @author Artem Shipilov (shipilov.89521049213@mail.ru)
+* @version $Id$
+* @since 21.03.2017
+*/
+package ru.job4j;
