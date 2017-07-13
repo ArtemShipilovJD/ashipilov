f333c5f31c4116865bb39db8e41c8ee8ec54d098
artem <artem>
2017-07-13 21:34:24 +0300

    3.1 a maximum of two

---
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -0,0 +1,14 @@
+package ru.job4j.max
+
+public class Max {
+
+     public int max(int first, int second);
+     
+     int first = 2;
+
+     int second = 3;
+
+     int max = int first > int second ? int first : int second;
+
+}
+     
