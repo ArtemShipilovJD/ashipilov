742041654295d34da9f8a43aaa228c096b887a78
artem <artem>
2017-11-30 22:17:04 +0300

    поменял расположение тестов

--- chapter_001/src/main/java/ru/job4j/condition/condition.java
+++ chapter_001/src/main/java/ru/job4j/condition/condition.java
@@ -14,15 +14,15 @@ public class DummyBot {
     public String answer(String question) {
         String rs1="Это ставит меня в тупик. Спросите другой вопрос.";
 
-        if("Привет, Бот.".equals(question)){
+        if("Привет, Бот.".equals(question)) {
 			rs1="Привет, умник.";
 			
-        } else if("Пока.".equals(question)){ 
+        } else if("Пока.".equals(question)) { 
 		    rs1="До скорой встречи.";
 			
         }
                 return rs1;
-	    }
+	}
         
     
 }
