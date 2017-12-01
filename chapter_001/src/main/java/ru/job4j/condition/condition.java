eaf455cfcf2aa4dd555e34965dfb3f2909b4ea8a
artem <artem>
2017-12-01 11:48:54 +0300

    выровнял конструкцию кода

--- chapter_001/src/main/java/ru/job4j/condition/condition.java
+++ chapter_001/src/main/java/ru/job4j/condition/condition.java
@@ -12,17 +12,19 @@ public class DummyBot {
      * @return Ответ.
      */
     public String answer(String question) {
-        String rs1="Это ставит меня в тупик. Спросите другой вопрос.";
+           String rs1="Это ставит меня в тупик. Спросите другой вопрос.";
 
-        if("Привет, Бот.".equals(question)) {
-			rs1="Привет, умник.";
+           if("Привет, Бот.".equals(question)) {
+	   rs1="Привет, умник.";
 			
-        } else if("Пока.".equals(question)) { 
-		    rs1="До скорой встречи.";
+         } else 
+
+               if("Пока.".equals(question)) { 
+	       rs1="До скорой встречи.";
 			
-        }
-                return rs1;
-	}
+         }
+               return rs1;
+         }
         
     
 }
